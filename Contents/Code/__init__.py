958d4fb8b1193ab9f1d4fb225fe671dbf8a24fea
Gerk <mark@brainwebsolutions.com>
2014-01-26 16:10:44 -0500

    large additions, more videos to choose from

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,4 +1,5 @@
 NAME="Golf Channel"
+DEFAULT_THUMB = "http://resources-cdn.plexapp.com/image/source/com.plexapp.plugins.golfchannel.jpg"
 
 ####################################################################################################
 def Start():
@@ -9,31 +10,106 @@ def Start():
 ####################################################################################################
 @handler('/video/golfchannel', NAME)
 def MainMenu():
-        return ShowList()
+        oc = ObjectContainer()
+        
+        oc.add(DirectoryObject(key=Callback(FeaturedShows), title="Featured Shows", thumb=DEFAULT_THUMB, summary="Shows currently featured on golfchannel website.  May or may not have full episodes."))
+        oc.add(DirectoryObject(key=Callback(FullEpisodes), title="Shows With Full Episodes", thumb=DEFAULT_THUMB, summary="Shows listed as having full episodes on golfchannel site."))
+        oc.add(DirectoryObject(key=Callback(FeaturedVideos), title="Featured Videos", thumb=DEFAULT_THUMB, summary="Videos featured on the golfchannel website."))
+        oc.add(DirectoryObject(key=Callback(LatestVideos, start=0), title="Latest Videos", thumb=DEFAULT_THUMB, summary="Latest videos posted on the golfchannel website."))
+        
+        return oc
 
 ####################################################################################################
-@route("/video/golfchannel/getshows")
-def ShowList():
+@route("/video/golfchannel/fullepisodes")
+def FullEpisodes():
 	oc = ObjectContainer()
 	
-	page = HTML.ElementFromURL("http://www.golfchannel.com/tv")
+	page = HTML.ElementFromURL("http://www.golfchannel.com")
 	
-	for show in page.xpath("//div[contains(@class,'view-display-id-all_golf_channel_full_episodes')]//div[contains(@class,'views-row')]"):
-		thumb = show.xpath(".//img/@src")[0]
-		url = "http://www.golfchannel.com%s" % show.xpath(".//a/@href")[0]
-		Log("url: "+url)
-		title = show.xpath(".//div[contains(@class,'views-field-title')]//a/text()")[0]
-		summary = show.xpath(".//div[contains(@class,'views-field-body')]/div[contains(@class,'field-content')]/text()")[0]
-		Log("show: "+title)
+	for show in page.xpath("//h2[contains(.,'Full Episodes') or contains(.,'More full episodes')]/following-sibling::div//li/a"):
+		title = show.xpath("./text()")[0]
+		url = "http://www.golfchannel.com%s" % show.xpath("./@href")[0]
+
 		oc.add(DirectoryObject(
 			key = Callback(GetVideos,url=url,show=title),
 			title = title, 
-			thumb = Resource.ContentsOfURLWithFallback(thumb),
-			summary=summary
-			
+			thumb = DEFAULT_THUMB
 		))
 		
 	return oc
+
+####################################################################################################
+@route("/video/golfchannel/featuredshows")
+def FeaturedShows():
+	oc = ObjectContainer()
+	
+	page = HTML.ElementFromURL("http://www.golfchannel.com")
+	
+	for show in page.xpath("//h2[contains(.,'Featured Shows')]/following-sibling::div//li/a"):
+		title = show.xpath("./text()")[0]
+		url = "http://www.golfchannel.com%s" % show.xpath("./@href")[0]
+		
+		oc.add(DirectoryObject(
+			key = Callback(GetVideos,url=url,show=title),
+			title = title, 
+			thumb = DEFAULT_THUMB
+		))
+		
+	return oc
+
+####################################################################################################
+@route("/video/golfchannel/featuredvideos")
+def FeaturedVideos():
+	oc = ObjectContainer()
+
+	page = HTML.ElementFromURL("http://www.golfchannel.com/media")
+	for show in page.xpath("//div[contains(@class,'featured-video-photos')]//div[contains(@class,'views-row')]"):		
+		# now we have to check to make sure this is a video
+		# if the video xpath fails it's not a video!
+		try: 
+			video = show.xpath(".//span[contains(@class,'video')]")[0]		
+			title = show.xpath(".//div[contains(@class,'views-field-title')]//a/text()")[0]
+			url = "http://www.golfchannel.com%s" % show.xpath(".//a/@href")[0]
+			thumb = show.xpath(".//img/@src")[0]
+			oc.add(VideoClipObject(
+				url = url,
+				title = title,
+				#thumb = Resource.ContentsOfURLWithFallback(thumb)
+				thumb = thumb
+			))
+		except:
+			continue
+
+	return oc
+
+####################################################################################################
+@route("/video/golfchannel/latestvideos", start=int)
+def LatestVideos(start=0):
+	oc = ObjectContainer()
+	
+	page = HTML.ElementFromURL("http://www.golfchannel.com/search/?&q=&submitSearch=+&mediatype=Video&start=%i" % start)
+	
+	for video in page.xpath("//li[contains(@class,'ez-Video')]"):
+		Log(video)
+		thumb = video.xpath(".//img/@src")[0]
+		title = video.xpath(".//div[contains(@class,'ez-main')]/a/text()")[0]
+		url = video.xpath(".//div[contains(@class,'ez-main')]/a/@href")[0]
+		summary = video.xpath(".//p[contains(@class,'ez-desc')]/text()")[0]
+		originally_available_at = Datetime.ParseDate(video.xpath(".//p[contains(@class,'ez-date')]/text()")[0]).date()
+
+		oc.add(VideoClipObject(
+			url = url,
+			title = title,
+			thumb = thumb, 
+			summary = summary, 
+			originally_available_at = originally_available_at
+		))
+		
+	if start < 499:
+		oc.add(NextPageObject(key=Callback(LatestVideos, start=int(start+10)), title="Next Page"))
+
+	return oc
+
 ####################################################################################################
 @route("/video/golfchannel/getvideos")
 def GetVideos(url, show):
@@ -42,7 +118,6 @@ def GetVideos(url, show):
 	page = HTML.ElementFromURL(url)
 	for show in page.xpath("//div[contains(@class,'pane-gfc-tv-shows-individual-carrousels')]//li[contains(@class,'views-row')]//div[contains(@class,'views-field-title')]//a"):		
 		# now we have to check to make sure this is a video
-		# watermak_video_content_type
 		# if the video xpath fails it's not a video!
 		try: 
 			video = show.xpath("../../..//span[contains(@class,'watermak_video_content_type')]")[0]		
@@ -52,7 +127,8 @@ def GetVideos(url, show):
 			oc.add(VideoClipObject(
 				url = url,
 				title = title,
-				thumb = Resource.ContentsOfURLWithFallback(thumb)
+				#thumb = Resource.ContentsOfURLWithFallback(thumb)
+				thumb = thumb
 			))
 		except:
 			continue
